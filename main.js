274a186ebfcaf1e33052d4bee22f79698e48c8e1
uomma <marcelloleone975@gmail.com>
2023-12-08 18:01:33 +0100

    html+css

--- main.js
+++ main.js
@@ -5,19 +5,19 @@ const games = [
         img: 'img/gta5.jpg',
         title: 'gtaV',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis velit fugiat doloribus laboriosam eos, sunt quasi ad dolor harum repellat. In, illo. Sint et ex iste dicta vitae, fugiat deleniti!'
-    },  {
+    }, {
         img: 'fortnite.jpg',
         title: 'fortnite',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis velit fugiat doloribus laboriosam eos, sunt quasi ad dolor harum repellat. In, illo. Sint et ex iste dicta vitae, fugiat deleniti!'
-    },  {
+    }, {
         img: 'halo4.png',
         title: 'halo4',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis velit fugiat doloribus laboriosam eos, sunt quasi ad dolor harum repellat. In, illo. Sint et ex iste dicta vitae, fugiat deleniti!'
-    },  {
+    }, {
         img: 'ufc4.jpg',
         title: 'ufc4',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis velit fugiat doloribus laboriosam eos, sunt quasi ad dolor harum repellat. In, illo. Sint et ex iste dicta vitae, fugiat deleniti!'
-    },  {
+    }, {
         img: 'f1.png',
         title: 'f1',
         text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis velit fugiat doloribus laboriosam eos, sunt quasi ad dolor harum repellat. In, illo. Sint et ex iste dicta vitae, fugiat deleniti!'
